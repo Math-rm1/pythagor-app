d33b67b496ce0f32e0660402c698899d9c1f2979
Math-rm1 <matheus.r.mariano@hotmail.com>
2022-07-13 20:25:05 -0300

    feat(project): init repo

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Link } from '@mui/material'
+
+export function App() {
+  return (
+    <>
+      <Link
+        href="https://www.flaticon.com/free-icons/pythagoras"
+        title="pythagoras icons"
+      >
+        Pythagoras icon created by Freepik - Flaticon
+      </Link>
+    </>
+  )
+}
