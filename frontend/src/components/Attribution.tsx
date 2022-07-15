450741170d652db9709879c68a47d9c2f5fd0c55
Math-rm1 <matheus.r.mariano@hotmail.com>
2022-07-15 11:52:46 -0300

    style(frontend): add margin bottom to element

--- frontend/src/components/Attribution.tsx
+++ frontend/src/components/Attribution.tsx
@@ -14,6 +14,7 @@ export function Attribution() {
           '&:hover': {
             textDecoration: 'underline',
           },
+          '&:last-child': { marginBottom: '1rem' },
         },
       }}
       component="footer"
